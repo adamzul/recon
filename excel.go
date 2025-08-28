9bad12a29cc7ea1a11dbf41f2d3d31d84c13c8ef
adam zulkarnain <adam.zulkarnain@stockbit.com>
2025-08-28 22:47:28 +0700

    handle total summary

--- excel.go
+++ excel.go
@@ -12,6 +12,41 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
+func writeTotalKeyValueToExcel(path, sheet string, total Total) error {
+	f, err := excelize.OpenFile(path)
+	if err != nil {
+		f = excelize.NewFile()
+	}
+
+	index, err := f.GetSheetIndex(sheet)
+	if err != nil {
+		return err
+	}
+
+	if index == -1 {
+		f.NewSheet(sheet)
+	}
+
+	// key-value pairs
+	rows := [][]interface{}{
+		{"Total Amount Transactions", total.TotalAmountTransactions},
+		{"Total Amount Bank Statements", total.TotalAmountBankStatements},
+		{"Total Matched", total.TotalMatched},
+		{"Total Unmatched", total.TotalUnmatched},
+		{"Total Processed", total.TotalProcessed},
+		{"Total Amount Dicrepancy", total.TotalAmountTransactions - total.TotalAmountBankStatements},
+	}
+
+	for i, row := range rows {
+		for j, v := range row {
+			cell, _ := excelize.CoordinatesToCellName(j+1, i+1)
+			f.SetCellValue(sheet, cell, v)
+		}
+	}
+
+	return f.SaveAs(path)
+}
+
 func writeTransactionsToExcel(path string, transactions []Transaction, sheet string) error {
 	f, err := excelize.OpenFile(path) // open existing file
 	if err != nil {
--- main.go
+++ main.go
@@ -53,6 +53,14 @@ func (b *BankStatementGroup) SetAppearMultiple() {
 	b.AppearMultiple = true
 }
 
+type Total struct {
+	TotalAmountTransactions   float64
+	TotalAmountBankStatements float64
+	TotalMatched              int
+	TotalUnmatched            int
+	TotalProcessed            int
+}
+
 func main() {
 	var transactionPath, bankStatementPaths string
 	flag.StringVar(&transactionPath, "transaction-path", "", "transactions CSV file path")
@@ -65,6 +73,8 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+
+	total := Total{TotalProcessed: len(transactions)}
 
 	bankStatementMap := map[float64]*BankStatementGroup{}
 	bankStatementPathArray := strings.Split(bankStatementPaths, ",")
@@ -80,17 +90,21 @@ func main() {
 				bankStatementMap[statement.Amount] = &BankStatementGroup{}
 			}
 			bankStatementMap[statement.Amount].Add(statement)
+			total.TotalAmountBankStatements += statement.Amount
 		}
 	}
 
 	//recon
-	transactions = lo.Filter(transactions, func(t Transaction, i int) bool {
+	transactionDiscrepancies := lo.Filter(transactions, func(t Transaction, i int) bool {
+		total.TotalAmountTransactions += t.Amount
 		bankStatements := bankStatementMap[t.Amount]
 		if bankStatements != nil && len(bankStatements.BankStatements) > 0 {
 			bankStatementMap[t.Amount].Shift()
+			total.TotalMatched++
 			// remove the transaction if it has a matching bank statement
 			return false
 		}
+		total.TotalUnmatched++
 		return true
 	})
 
@@ -103,11 +117,15 @@ func main() {
 				}
 				bankStatementDisrepancy[statement.Bank].Add(statement)
 				bankStatementDisrepancy[statement.Bank].SetAppearMultiple(group.AppearMultiple)
+				total.TotalProcessed++
+				total.TotalUnmatched++
 			}
 		}
 	}
 
-	writeTransactionsToExcel("recon.xlsx", transactions, "transaction")
+	writeTotalKeyValueToExcel("recon.xlsx", "summary", total)
+
+	writeTransactionsToExcel("recon.xlsx", transactionDiscrepancies, "transaction")
 	for bank, group := range bankStatementDisrepancy {
 		writeBankStatementsToExcel("recon.xlsx", group.Statements, group.AppearMultiple, bank)
 	}
